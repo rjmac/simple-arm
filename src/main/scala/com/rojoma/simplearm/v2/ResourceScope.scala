5fe19f3b2bcd1ac0640155fc809aa330da97828b
Robert J. Macomber <robertm@dilithium>
2019-09-25 14:07:03 -0700

    Don't self-suppress exceptions
    Not sure how this is happening, but it seems to be, somewhere

--- src/main/scala/com/rojoma/simplearm/v2/ResourceScope.scala
+++ src/main/scala/com/rojoma/simplearm/v2/ResourceScope.scala
@@ -185,7 +185,7 @@ final class ResourceScope(val name: String = ResourceScope.anonymousScopeName) {
           case control: ControlThrowable =>
             throw control
           case t2: Throwable =>
-            t.addSuppressed(t2)
+            if(t ne t2) t.addSuppressed(t2)
         }
         throw t
     }
@@ -278,7 +278,7 @@ final class ResourceScope(val name: String = ResourceScope.anonymousScopeName) {
               continueClosingAbnormally(i, cause)
               throw control
             case t: Throwable =>
-              cause.addSuppressed(t)
+              if(cause ne t) cause.addSuppressed(t)
           }
         }
       }
@@ -352,7 +352,7 @@ final class ResourceScope(val name: String = ResourceScope.anonymousScopeName) {
           closeAbnormally(cause) // finish closing
           throw control
         case t: Throwable =>
-          cause.addSuppressed(t)
+          if(cause ne t) cause.addSuppressed(t)
       }
     }
   }
@@ -383,7 +383,7 @@ final class ResourceScope(val name: String = ResourceScope.anonymousScopeName) {
           closeImpl(tmpScope, Some(cause))
         } catch {
           case t: Throwable =>
-            cause.addSuppressed(t)
+            if(cause ne t) cause.addSuppressed(t)
         }
         throw cause
     }
